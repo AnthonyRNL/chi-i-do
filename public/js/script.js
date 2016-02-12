d1f866693aed3f7111ff3f739b2aa6e6fb436509
github username <github email address>
2016-02-12 03:03:13 -0500

    media test

--- public/js/script.js
+++ public/js/script.js
@@ -20,18 +20,25 @@ $(document).ready(function(){
   })
 
   //event for the about container
+  $($("#toggle").children()).on("click", function(e){
+    toggleNav()
+  })
   $("#about").on("click", function(e){
     if(e.target.tagName === "H3"){
       $(e.currentTarget).children().children().removeClass("aboutActive")
       $(e.target).addClass("aboutActive")
       var index = $(e.target.parentNode).index()
       $("#aboutContainer").children().hide()
-      // debugger
       $($("#aboutContainer").children()[index]).css({"display": "block"})
     }
   })
 })
-
+var map;
+var mapOptions = {
+  zoom: 5,
+  center: new google.maps.LatLng(-34.397, 150.644)
+};
+map = new google.maps.Map(document.getElementById("map"), mapOptions)
 function toggleNav(){
   if($("#main").hasClass("display-nav")){
     $("#main").removeClass("display-nav")
